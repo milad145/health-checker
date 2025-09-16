d9ec73bcbe7605c1da5f1bd425c479f8b2c1a08e
milad145 <miladaslani1991@gmail.com>
2025-09-16 23:31:17 +0330

    initiate services before express initiation

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 import mongoose from 'mongoose';
+
 import config from './lib/config/index.js'
+
 import {initLogs, initSecurityLog, APP_LOG} from './lib/modules/logs.js';
+
+import SMSService from './lib/services/sms.js'
+import RabbitMQService from './lib/services/rabbitMQ.js';
+import RedisService from './lib/services/redis.js'
+
+const smsService = new SMSService(config)
+const rabbitMQService = new RabbitMQService(config)
+const redisService = new RedisService(config)
 
 initLogs(APP_LOG);
 initSecurityLog();
 // ===============================================
-import initServer from './lib/init.js';
+import initiateExpress from './lib/init.js';
 
-mongoose.set("strictQuery", false);
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(config.database);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        await rabbitMQService.init();
+        await redisService.init();
+        const dependencies = {config, smsService, redisService, rabbitMQService}
+        await initiateExpress(dependencies)
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
 
-mongoose.connect(config['database'])
-    .then(() => initServer())
-    .catch((error) => console.log('MongoDB connection error:', error));
+(() => connectDB())()
